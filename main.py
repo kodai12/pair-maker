02c6f2a13ac6d50acba09eea6b4de69ffd94b066
kodai12 <sakochi.kodai1209@gmail.com>
2019-01-07 22:23:19 +0900

    slack通知処理を実装

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import os
+
+
+class NotifyToSlack:
+    def __init__(self):
+        self.end_point = os.environ['SLACK_WEBHOOK_URL']
+
+    def run(self):
+        requests.post(self.end_point, data=json.dumps({
+            'text': u'Test',
+            'username': u'me',
+            'icon_emoji': u':money_with_wings:',
+            'link_names': 1
+        }))
+
+
+if __name__ == '__main__':
+    notify_to_slack = NotifyToSlack()
+    notify_to_slack.run()
