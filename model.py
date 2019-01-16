561bc54089670859942abb5cc4e81868c8a2f9e2
kodai12 <sakochi.kodai1209@gmail.com>
2019-01-16 22:22:27 +0900

    memberモデルの作成

--- model.py
+++ model.py
@@ -4,15 +4,46 @@ from more_itertools import chunked
 import random
 
 
-class Combination(metaclass=ABCMeta):
-    def __init__(self, combination: list):
-        self.combination = combination
+class MemberId:
+    def __init__(self, value: int):
+        self.value = value
+
+
+class MemberIndex:
+    def __init__(self, value: int):
+        self.value = value
+
+
+class MemberName:
+    def __init__(self, value: str):
+        self.value = value
 
 
 class Member:
-    def __init__(self, id: int, index: int):
+    def __init__(self, id: MemberId, index: MemberIndex, name: MemberName):
         self.id = id
         self.index = index
+        self.name = name
+
+    @staticmethod
+    def from_dict(member_dict: dict) -> Member:
+        return Member(
+                id=MemberId(member_dict['id']),
+                index=MemberIndex(member_dict['index']),
+                name=MemberName(member_dict['name'])
+                )
+
+    def to_dict(self) -> dict:
+        return {
+                'id': self.id.value,
+                'index': self.index.value,
+                'name': self.name.value
+                }
+
+
+class Combination(metaclass=ABCMeta):
+    def __init__(self, combination: list):
+        self.combination = combination
 
 
 class Pair(Combination):
