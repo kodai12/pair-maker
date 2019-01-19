b4d327c36fbea8d00a48585f6d13216619879d7b
kodai12 <sakochi.kodai1209@gmail.com>
2019-01-19 21:11:28 +0900

    handlerとusecaseを整理

--- handler.py
+++ handler.py
@@ -1,9 +1,30 @@
-import json
+import os
+
+from datasource import CsvDatasource
+from datasource import SlackDatasource
+from usecase import NotifyToSlack
 
 
 def hello(event, context):
     try:
-        pass
+        # create datasource
+        csv_datasource = CsvDatasource()
+        slack_datasource = SlackDatasource()
+
+        # get outside data
+        read_csv_file = os.environ['READ_CSV_FILE']
+        write_csv_file = os.environ['WRITE_CSV_FILE']
+
+        # create usecase
+        notify_to_slack = NotifyToSlack(
+                csv_datasource=csv_datasource,
+                slack_datasource=slack_datasource,
+                read_csv_file=read_csv_file,
+                write_csv_file=write_csv_file
+                )
+
+        # run usecase
+        notify_to_slack.run()
 
     except Exception as e:
         raise e
--- usecase.py
+++ usecase.py
@@ -5,18 +5,23 @@ from model import NotifyDate
 from model import Combination
 from model import create_combinations
 from model import update_combinations
-from datasource import SlackDatasource
 from datasource import CsvDatasource
+from datasource import SlackDatasource
 
 
 class NotifyToSlack:
-    def __init__(self):
-        pass
+    def __init__(self, csv_datasource: CsvDatasource,
+            slack_datasource: SlackDatasource,
+            read_csv_file: str,
+            write_csv_file: str):
+        self.csv_datasource = csv_datasource
+        self.slack_datasource = slack_datasource
+        self.read_csv_file = read_csv_file
+        self.write_csv_file = write_csv_file
 
     def run(self):
         # 組み合わせを取得
-        csv_datasource = CsvDatasource()
-        pair_index_list = csv_datasource.read('test.csv')  # TODO: 環境変数にする
+        pair_index_list = self.csv_datasource.read(self.read_csv_file)
         pairs: List[Combination] = create_combinations(pair_index_list)
         # 日付を取得
         today = datetime.today().date()
@@ -24,15 +29,15 @@ class NotifyToSlack:
         # 日付が更新可能な日付であれば前回の組み合わせから新しい組み合わせを作成
         notify_date = NotifyDate(today)
         if notify_date.is_holiday():
+            print('Today is Holiday!!')
             return  # TODO: 例外処理?
         new_pairs: List[Combination] = update_combinations(pairs)
 
         # 新しい組み合わせデータを永続化
-        csv_datasource.write('test2.csv', new_pairs)
+        self.csv_datasource.write(self.write_csv_file, new_pairs)
 
         # slackに通知
-        slack_datasource = SlackDatasource()
-        slack_datasource.post(pair_list=new_pairs)
+        self.slack_datasource.post(pair_list=new_pairs)
 
 
 if __name__ == '__main__':
