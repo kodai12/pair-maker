3ae4b36768ea4a4f6532f71213218372dde694ca
kodai12 <sakochi.kodai1209@gmail.com>
2019-01-20 15:08:54 +0900

    update_combination_listメソッドの修正

--- model/combination.py
+++ model/combination.py
@@ -22,6 +22,7 @@ class Combination(metaclass=ABCMeta):
     def to_dict() -> dict:
         pass
 
+
 class Pair(Combination):
     def __init__(self, index: CombinationIndex, memberList: List[Member]):
         self.index = index
@@ -94,30 +95,30 @@ class CombinationList:
         self.values = values
 
     def update_combination_list(self) -> 'CombinationList':
-        index_list_1 = []
-        remaining_list = []
+        index_stack = []
+        remaining_stack = []
         (random_index, remain_index) = self.__get_random_index()
         for combination in self.values:
             # ペアの片方は元のペアに残したままでもう片方はremainingにappendしておく
             if isinstance(combination, Pair):
                 (move_member, stay_member) = combination.divide_member()
-                index_list_1.append([stay_member])
-                remaining_list.append({
-                    'pair_index': combination.index.value,
+                index_stack.append([stay_member])
+                remaining_stack.append({
+                    'combination_index': combination.index.value,
                     'member': move_member
                     })
-            # singleの場合はrandom(1番目か2番目)にリストにinsertしておく
+            # singleの場合はランダム(1番目か2番目)にリストにinsertしておく
             if isinstance(combination, Single):
-                index_list_1[random_index].append(combination.member)
+                index_stack[random_index].append(combination.member)
 
         # remainingに残ったメンバーをいずれかのペアにinsertする
-        for remaining in remaining_list:
-            if remaining['pair_index'] == random_index:
-                index_list_1[remain_index].append(remaining['member'])
+        for remaining in remaining_stack:
+            if remaining['combination_index'] == random_index:
+                index_stack[remain_index].append(remaining['member'])
             else:
-                index_list_1.append([remaining['member']])
+                index_stack.append([remaining['member']])
 
-        index_list_dict = [i.to_dict() for i in chain.from_iterable(index_list_1)]
+        index_list_dict = [i.to_dict() for i in chain.from_iterable(index_stack)]
         return create_combinations(index_list_dict)
 
     def __get_random_index(self) -> tuple:
